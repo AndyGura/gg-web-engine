f1590be78dfae3fa923d0b521860e1a15799714d
Andy <andy.gura.jd@gmail.com>
2023-01-06 07:22:11 +0200

    updated three.js; added workaround for light intensity, imported from glb

--- packages/three/src/impl/gg-3d-object-loader.ts
+++ packages/three/src/impl/gg-3d-object-loader.ts
@@ -1,11 +1,19 @@
 import { GgMeta, IGg3dObjectLoader } from '@gg-web-engine/core';
 import { Gg3dObject } from './gg-3d-object';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Light, Object3D } from 'three';
 
 export class Gg3dObjectLoader implements IGg3dObjectLoader {
   public async loadFromGgGlb(glbFile: ArrayBuffer, meta: GgMeta): Promise<Gg3dObject | null> {
     const loader: GLTFLoader = new GLTFLoader();
     const gltf = await loader.parseAsync(glbFile, '');
+    gltf.scene.traverse((obj: Object3D) => {
+      if (obj.type.endsWith('Light')) {
+        // TODO determine why it happens and fix in a correct way
+        console.warn('WORKAROUND: light intensity from GLB divided by 200.');
+        (obj as Light).intensity *= 0.005;
+      }
+    });
     return new Gg3dObject(gltf.scene);
   }
 }
