2f24c537086bb0fba08bb40b909fca5916e92ce4
Andy <andy.gura.jd@gmail.com>
2023-04-12 00:18:51 +0300

    fixed typo in local development setup script

--- etc/setup_local_development_env.sh
+++ etc/setup_local_development_env.sh
@@ -28,7 +28,7 @@ popd
 
 function fix_ammo_paths {
   sed -i 's/\/mini-signals/\/@gg-web-engine\/ammo\/node_modules\/mini-signals/' tsconfig.json
-  sed -i 's/"paths": {/"paths": {\n"ammo-js-typed": [".\/node_modules\/@gg-web-engine\/three\/node_modules\/ammo-js-typed"],/' tsconfig.json
+  sed -i 's/"paths": {/"paths": {\n"ammo-js-typed": [".\/node_modules\/@gg-web-engine\/ammo\/node_modules\/ammo-js-typed"],/' tsconfig.json
 }
 
 function fix_three_paths {
