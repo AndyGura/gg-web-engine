c18eaf58725eb6723289d616cdd61d533520c569
Andy <andy.gura.jd@gmail.com>
2024-04-08 21:30:28 +0300

    fixed pixi integration after library upgrade

--- packages/pixi/src/components/pixi-renderer.component.ts
+++ packages/pixi/src/components/pixi-renderer.component.ts
@@ -2,9 +2,12 @@ import { Gg2dWorld, IRenderer2dComponent, Point2, RendererOptions } from '@gg-we
 import { Application } from 'pixi.js';
 import { PixiSceneComponent } from './pixi-scene.component';
 import { PixiVisualTypeDocRepo2D } from '../types';
+import { first, Subject } from 'rxjs';
 
 export class PixiRendererComponent extends IRenderer2dComponent<PixiVisualTypeDocRepo2D> {
   public readonly application: Application;
+  private initialized: boolean = false;
+  private onInitialized$: Subject<void> = new Subject();
 
   constructor(
     public readonly scene: PixiSceneComponent,
@@ -12,8 +15,9 @@ export class PixiRendererComponent extends IRenderer2dComponent<PixiVisualTypeDo
     options: Partial<RendererOptions> = {},
   ) {
     super(scene, canvas, options);
-    this.application = new Application({
-      view: canvas,
+    this.application = new Application();
+    this.application.init({
+      canvas,
       backgroundAlpha: this.rendererOptions.transparent ? 0 : 1,
       autoDensity: this.rendererOptions.forceResolution === undefined,
       resolution: this.rendererOptions.forceResolution || devicePixelRatio,
@@ -24,15 +28,23 @@ export class PixiRendererComponent extends IRenderer2dComponent<PixiVisualTypeDo
       // preventing ticks
       autoStart: false,
       sharedTicker: false,
+    }).then(() => {
+      // GG uses own ticker, disable pixi ticker for this renderer
+      this.application.ticker.stop();
+      this.application.ticker.destroy();
+      (this.application as any)._ticker = null!;
+      this.initialized = true;
+      this.onInitialized$.next();
+      this.onInitialized$.complete();
     });
-    // GG uses own ticker, disable pixi ticker for this renderer
-    this.application.ticker.stop();
-    this.application.ticker.destroy();
-    (this.application as any)._ticker = null!;
   }
 
   resizeRenderer(newSize: Point2): void {
-    this.application.renderer.resize(newSize.x, newSize.y);
+    if (this.initialized) {
+      this.application.renderer.resize(newSize.x, newSize.y);
+    } else {
+      this.onInitialized$.pipe(first()).subscribe(() => this.resizeRenderer(newSize));
+    }
   }
 
   addToWorld(world: Gg2dWorld<PixiVisualTypeDocRepo2D>): void {
@@ -44,7 +56,11 @@ export class PixiRendererComponent extends IRenderer2dComponent<PixiVisualTypeDo
   }
 
   render(): void {
-    this.application.render();
+    if (this.initialized) {
+      this.application.render();
+    } else {
+      this.onInitialized$.pipe(first()).subscribe(() => this.render());
+    }
   }
 
   dispose(): void {
