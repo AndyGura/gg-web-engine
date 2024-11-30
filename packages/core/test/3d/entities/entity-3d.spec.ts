732a441554177e08da749b101dce9ffe4449c7bf
Andy <andy.gura.jd@gmail.com>
2024-12-01 00:59:25 +0200

    improved FPS (#22)
    * improved FPS
    
    * fixed tests and binding regression

--- packages/core/test/3d/entities/entity-3d.spec.ts
+++ packages/core/test/3d/entities/entity-3d.spec.ts
@@ -6,42 +6,42 @@ describe(`Entity3d`, () => {
 
   describe(`constructor`, () => {
     it(`should pull position and rotation from body immediately`, () => {
-      const body = mock3DBody();
-      body.position = { x: 1, y: 2, z: 3 };
-      body.rotation = { x: 4, y: -1, z: 2, w: 3 };
-      const entity = new Entity3d(null, body);
+      const objectBody = mock3DBody();
+      objectBody.position = { x: 1, y: 2, z: 3 };
+      objectBody.rotation = { x: 4, y: -1, z: 2, w: 3 };
+      const entity = new Entity3d({ objectBody });
       expect(entity.position).toEqual({ x: 1, y: 2, z: 3 });
       expect(entity.rotation).toEqual({ x: 4, y: -1, z: 2, w: 3 });
     });
     it(`should pull position from object immediately if no body`, () => {
-      const object = mock3DObject();
-      object.position = { x: 1, y: 3, z: 2 };
-      object.rotation = { x: -1, y: 0, z: 1, w: 2 };
-      const entity = new Entity3d(object, null);
+      const object3D = mock3DObject();
+      object3D.position = { x: 1, y: 3, z: 2 };
+      object3D.rotation = { x: -1, y: 0, z: 1, w: 2 };
+      const entity = new Entity3d({ object3D });
       expect(entity.position).toEqual({ x: 1, y: 3, z: 2 });
       expect(entity.rotation).toEqual({ x: -1, y: 0, z: 1, w: 2 });
     });
     it(`when both object and body defined, should pull and apply position of the body`, () => {
-      const object = mock3DObject();
-      object.position = { x: 1, y: 3, z: 2 };
-      object.rotation = { x: -1, y: 0, z: 1, w: 2 };
-      const body = mock3DBody();
-      body.position = { x: 1, y: 2, z: 3 };
-      body.rotation = { x: 4, y: -1, z: 2, w: 3 };
-      const entity = new Entity3d(object, body);
+      const object3D = mock3DObject();
+      object3D.position = { x: 1, y: 3, z: 2 };
+      object3D.rotation = { x: -1, y: 0, z: 1, w: 2 };
+      const objectBody = mock3DBody();
+      objectBody.position = { x: 1, y: 2, z: 3 };
+      objectBody.rotation = { x: 4, y: -1, z: 2, w: 3 };
+      const entity = new Entity3d({ object3D, objectBody });
       expect(entity.position).toEqual({ x: 1, y: 2, z: 3 });
       expect(entity.rotation).toEqual({ x: 4, y: -1, z: 2, w: 3 });
-      expect(object.position).toEqual({ x: 1, y: 2, z: 3 });
-      expect(object.rotation).toEqual({ x: 4, y: -1, z: 2, w: 3 });
+      expect(object3D.position).toEqual({ x: 1, y: 2, z: 3 });
+      expect(object3D.rotation).toEqual({ x: 4, y: -1, z: 2, w: 3 });
     });
   });
 
   describe(`tick`, () => {
     it(`should pull position and rotation from body`, () => {
-      const body = mock3DBody();
-      const entity = new Entity3d(null, body);
-      body.position = { x: 4, y: -1, z: 0 };
-      body.rotation = { x: 2, y: 3, z: 1, w: -5 };
+      const objectBody = mock3DBody();
+      const entity = new Entity3d({ objectBody });
+      objectBody.position = { x: 4, y: -1, z: 0 };
+      objectBody.rotation = { x: 2, y: 3, z: 1, w: -5 };
       expect(entity.position).toEqual({ x: 0, y: 0, z: 0 });
       expect(entity.rotation).toEqual({ x: 0, y: 0, z: 0, w: 1 });
       entity.tick$.next([0, 0]);
@@ -49,27 +49,27 @@ describe(`Entity3d`, () => {
       expect(entity.rotation).toEqual({ x: 2, y: 3, z: 1, w: -5 });
     });
     it(`should pull position and rotation from body and overwrite object transform`, () => {
-      const body = mock3DBody();
-      const object = mock3DObject();
-      const entity = new Entity3d(object, body);
-      body.position = { x: 4, y: -1, z: 0 };
-      body.rotation = { x: 2, y: 3, z: 1, w: -5 };
-      object.position = { x: 1, y: 3, z: 2 };
-      object.rotation = { x: -1, y: 0, z: 1, w: 2 };
+      const objectBody = mock3DBody();
+      const object3D = mock3DObject();
+      const entity = new Entity3d({ object3D, objectBody });
+      objectBody.position = { x: 4, y: -1, z: 0 };
+      objectBody.rotation = { x: 2, y: 3, z: 1, w: -5 };
+      object3D.position = { x: 1, y: 3, z: 2 };
+      object3D.rotation = { x: -1, y: 0, z: 1, w: 2 };
       expect(entity.position).toEqual({ x: 0, y: 0, z: 0 });
       expect(entity.rotation).toEqual({ x: 0, y: 0, z: 0, w: 1 });
       entity.tick$.next([0, 0]);
       expect(entity.position).toEqual({ x: 4, y: -1, z: 0 });
       expect(entity.rotation).toEqual({ x: 2, y: 3, z: 1, w: -5 });
-      expect(object.position).toEqual({ x: 4, y: -1, z: 0 });
-      expect(object.rotation).toEqual({ x: 2, y: 3, z: 1, w: -5 });
+      expect(object3D.position).toEqual({ x: 4, y: -1, z: 0 });
+      expect(object3D.rotation).toEqual({ x: 2, y: 3, z: 1, w: -5 });
     });
   });
 
   describe(`dispose`, () => {
     it(`should not fail if disposing twice (happens for sub-entities when disposing whole world)`, () => {
-      const body = mock3DBody();
-      const entity = new Entity3d(null, body);
+      const objectBody = mock3DBody();
+      const entity = new Entity3d({ objectBody });
       entity.dispose();
       expect(() => entity.dispose()).not.toThrow(Error);
     });
@@ -77,13 +77,13 @@ describe(`Entity3d`, () => {
 
   describe('visible', () => {
     test('should set the visible property', () => {
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       gg3dEntity.visible = false;
       expect(gg3dEntity.visible).toBe(false);
     });
 
     test('should update visibility', () => {
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       gg3dEntity.visible = false;
       gg3dEntity.updateVisibility();
       expect(gg3dEntity.object3D!.visible).toBe(false);
@@ -97,7 +97,7 @@ describe(`Entity3d`, () => {
       const parentEntity2 = { visible: true, parent: parentEntity1 } as any;
       const parentEntity3 = { visible: true, parent: parentEntity2 } as any;
 
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       gg3dEntity.parent = parentEntity3;
       expect(gg3dEntity.worldVisible).toBe(true);
     });
@@ -107,7 +107,7 @@ describe(`Entity3d`, () => {
       const parentEntity2 = { visible: false, parent: parentEntity1 } as any;
       const parentEntity3 = { visible: true, parent: parentEntity2 } as any;
 
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       gg3dEntity.parent = parentEntity3;
       expect(gg3dEntity.worldVisible).toBe(false);
     });
@@ -115,23 +115,23 @@ describe(`Entity3d`, () => {
 
   describe('updateVisibility', () => {
     test('should update object3D visibility when worldVisible is true', () => {
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       gg3dEntity.visible = true;
       gg3dEntity.updateVisibility();
       expect(gg3dEntity.object3D!.visible).toBe(true);
     });
 
     test('should not update object3D visibility when worldVisible is false', () => {
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       gg3dEntity.visible = false;
       gg3dEntity.updateVisibility();
       expect(gg3dEntity.object3D!.visible).toBe(false);
     });
 
     test('should update visibility of children', () => {
-      const a = new Entity3d(mock3DObject());
-      const b = new Entity3d(mock3DObject());
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const a = new Entity3d({ object3D: mock3DObject() });
+      const b = new Entity3d({ object3D: mock3DObject() });
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       a.addChildren(b);
       b.addChildren(gg3dEntity);
       expect(gg3dEntity.object3D!.visible).toBe(true);
@@ -141,10 +141,10 @@ describe(`Entity3d`, () => {
     });
 
     test('should update visibility of children even if intermediate child is not renderable', () => {
-      const a = new Entity3d(mock3DObject());
+      const a = new Entity3d({ object3D: mock3DObject() });
       const b = new AnimationMixer(null!);
-      const c = new Entity3d(mock3DObject());
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const c = new Entity3d({ object3D: mock3DObject() });
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       a.addChildren(b);
       b.addChildren(c);
       c.addChildren(gg3dEntity);
@@ -155,17 +155,17 @@ describe(`Entity3d`, () => {
     });
 
     test('should be called when adding as child to entity', () => {
-      const a = new Entity3d(mock3DObject());
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const a = new Entity3d({ object3D: mock3DObject() });
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       const call = jest.spyOn(gg3dEntity, 'updateVisibility');
       a.addChildren(gg3dEntity);
       expect(call).toHaveBeenCalledTimes(1);
     });
 
     test('should be called when adding own parent as child to entity', () => {
-      const a = new Entity3d(mock3DObject());
-      const b = new Entity3d(mock3DObject());
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const a = new Entity3d({ object3D: mock3DObject() });
+      const b = new Entity3d({ object3D: mock3DObject() });
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       b.addChildren(gg3dEntity);
       const call = jest.spyOn(gg3dEntity, 'updateVisibility');
       a.addChildren(b);
@@ -173,9 +173,9 @@ describe(`Entity3d`, () => {
     });
 
     test('should be called when adding own non-renderable parent as child to entity', () => {
-      const a = new Entity3d(mock3DObject());
+      const a = new Entity3d({ object3D: mock3DObject() });
       const b = new AnimationMixer(null!);
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       b.addChildren(gg3dEntity);
       const call = jest.spyOn(gg3dEntity, 'updateVisibility');
       a.addChildren(b);
@@ -183,8 +183,8 @@ describe(`Entity3d`, () => {
     });
 
     test('should be called when removing as child from entity', () => {
-      const a = new Entity3d(mock3DObject());
-      const gg3dEntity = new Entity3d(mock3DObject());
+      const a = new Entity3d({ object3D: mock3DObject() });
+      const gg3dEntity = new Entity3d({ object3D: mock3DObject() });
       a.addChildren(gg3dEntity);
       const call = jest.spyOn(gg3dEntity, 'updateVisibility');
       a.removeChildren([gg3dEntity]);
