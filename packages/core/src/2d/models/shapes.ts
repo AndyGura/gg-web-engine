f180d2e29549dfe92f1b62db69a34365a9b0f25f
andy <andy.gura.jd@gmail.com>
2026-09-27 00:24:23 +0300

    outlined new 2d shapes

--- packages/core/src/2d/models/shapes.ts
+++ packages/core/src/2d/models/shapes.ts
@@ -2,8 +2,16 @@ import { Point2 } from '../../base';
 import { Body2DOptions } from './body-options';
 
 export type Shape2DDescriptor = { collisionMargin?: number } & (
-  { shape: 'SQUARE'; dimensions: Point2 } | { shape: 'CIRCLE'; radius: number }
-);
-// TODO add more shapes
+  | { shape: 'SQUARE'; dimensions: Point2 } // TODO rename to "BOX"
+  | { shape: 'CIRCLE'; radius: number }
+  // TODO implement these
+  // | { shape: 'CAPSULE'; radius: number; centersDistance: number }
+  // | { shape: 'POLYGON'; vertices: Point2[] }
+  // | { shape: 'COMPOUND'; children: {
+  //   position?: Point2;
+  //   rotation?: number;
+  //   shape: Shape2DDescriptor;
+  // }[] }
+  );
 
 export type BodyShape2DDescriptor = { shape: Shape2DDescriptor; body: Partial<Body2DOptions> };
